4f0fd842694a57eee84ba8ba20a6ec64a3000364
Stefan Vizzari <svizzari@zendesk.com>
2017-08-03 16:33:42 +1000

    refactor(recast): use node visitation to find and mutate the v1 source code

--- src/steps/migrate_app_js.ts
+++ src/steps/migrate_app_js.ts
@@ -8,20 +8,52 @@ export default async (options: Map<string, any>) => {
   const dest = options.get("dest");
   const editor = options.get("editor");
   const appJS = editor.read(`${src}/app.js`);
+
+  // Parse all of the v1 app.js Javascript into an AST
   const ast = recast.parse(appJS);
-  // TODO: Traverse the AST properly to find the relevant return statement
-  const returnStatement =
-    ast.program.body[0].expression.callee.body.body[0].argument.properties;
 
-  ast.program = b.program([
-    b.variableDeclaration("const", [
-      b.variableDeclarator(
-        b.identifier("MigratedApp"),
-        b.objectExpression(returnStatement)
-      )
-    ])
-  ]);
+  // Traverse the AST to find the v1 `return` statement that actually
+  // returns the app subclass
+  recast.types.visit(ast, {
+    visitReturnStatement(path) {
+      const node = path.node;
+      if (this.hasAppEventsHash(node)) {
+        // This is ugly, but seemingly necessary to find the IIFE that
+        // wraps the main return statement
+        const outerExpression = path.parent.parent.parent.parent;
+        if (n.ExpressionStatement.assert(outerExpression.node)) {
+          // Create a new code structure that assigns the result of calling the IIFE
+          // to the new MigratedApp constant
+          const newNode = b.variableDeclaration("const", [
+            b.variableDeclarator(
+              b.identifier("MigratedApp"),
+              outerExpression.node.expression
+            )
+          ]);
+          // Replace the IIFE with the assignment
+          outerExpression.replace(newNode);
+          // That's all we need to do with the AST for now
+          // Later steps _might_ do some further mutation
+          this.abort();
+        }
+      }
+      this.traverse(path);
+    },
+    // This helper method checks whether the return statement node
+    // being visited has an events hash declaration within
+    hasAppEventsHash(node) {
+      if (!n.ObjectExpression.check(node.argument)) {
+        return false;
+      }
+      const events = node.argument.properties.find(element => {
+        return element.key.name === "events";
+      });
+      return events && n.ObjectExpression.assert(events.value);
+    }
+  });
 
+  // FIXME: Move writing the AST to output Javascript to
+  // another step, if steps after this will mutate the AST again
   const code: string = recast.prettyPrint(ast, {
     tabWidth: 2
   }).code;
@@ -31,4 +63,6 @@ export default async (options: Map<string, any>) => {
   editor.copyTpl(indexTpl, destJS, { code });
 
   editor.delete(`${dest}/src/javascripts/legacy_app.js`);
+
+  return options.set("ast", ast);
 };
