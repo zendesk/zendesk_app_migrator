d6036497b0d83dd05341f2d3fbee77fd52b1a20d
Stefan Vizzari <svizzari@zendesk.com>
2017-08-04 12:48:17 +1000

    refactor(images): glob all of the image types the scaffold handles

--- src/steps/migrate_images.ts
+++ src/steps/migrate_images.ts
@@ -6,6 +6,6 @@ export default async (options: Map<string, any>) => {
   const editor = options.get("editor");
 
   // Copy all image files across
-  const imageGlob: string = "{jpeg,jpg,gif,png,svg}";
+  const imageGlob: string = "{jp?(e)g,jpg,gif,png,svg,woff?(2)}";
   editor.copy(`${src}/assets/**/*.${imageGlob}`, `${dest}/src/images/`);
 };
