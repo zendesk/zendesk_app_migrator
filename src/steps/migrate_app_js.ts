e073371ceb5dd0f0a8981795ca28f5fb0963f40a
Stefan Vizzari <svizzari@zendesk.com>
2017-08-08 15:23:40 +1000

    refactor(migrate_common_js): inject common js file imports, if they exist

--- src/steps/migrate_app_js.ts
+++ src/steps/migrate_app_js.ts
@@ -1,5 +1,6 @@
 import * as recast from "recast";
 import { Map } from "immutable";
+import { uniqueId } from "lodash";
 const n = recast.types.namedTypes;
 const b = recast.types.builders;
 
@@ -7,7 +8,16 @@ export default async (options: Map<string, any>) => {
   const src = options.get("src");
   const dest = options.get("dest");
   const editor = options.get("editor");
+  const hasCommonJS = options.get("hasCommonJS");
   const appJS = editor.read(`${src}/app.js`);
+
+  let code: string = `
+    (function() {
+      return {
+        /* no-op */
+      };
+    }());
+  `;
 
   // Parse all of the v1 app.js Javascript into an AST
   const ast = recast.parse(appJS);
@@ -30,20 +40,45 @@ export default async (options: Map<string, any>) => {
     }
   });
 
+  let imports = "";
+  // Check whether the migrate_common_js step discovered some Common JS files
+  if (hasCommonJS) {
+    recast.types.visit(ast, {
+      visitCallExpression(path) {
+        const node = path.node;
+        // Check whether there are any calls to `require("some_module")`
+        if (n.Identifier.check(node.callee) && node.callee.name === "require") {
+          // Keep the path to the module
+          const modulePath: string = node.arguments[0].value;
+          // The path may actually include subfolders, i.e. `require("a/b/some_module")`
+          // Use the last part of the path as the module name
+          const moduleName: string = modulePath.split("/").pop();
+          // Make sure the module name is _more_ unique by adding a unique number
+          const uniqueModuleName: string = uniqueId(`${moduleName}_`);
+          // Add an import statement for the module.
+          // Change the path to be relative so that Webpack can find the module without
+          // needing any further module path configuration
+          imports += `import * as ${uniqueModuleName} from "./lib/${modulePath}";\n`;
+          // Replace the original call expression with a reference to the import
+          path.replace(b.identifier(uniqueModuleName));
+        }
+        this.traverse(path);
+      }
+    });
+  }
+
   if (topLevelReturnStatementPath) {
     const iifePath = topLevelReturnStatementPath.parent.parent.parent.parent;
     if (n.ExpressionStatement.assert(iifePath.node)) {
       ast.program = b.program([iifePath.node]);
     }
+    // Generate the final JavaScript for the app subclass definition
+    code = recast.prettyPrint(ast, {
+      tabWidth: 2
+    }).code;
   }
-
-  // FIXME: Move writing the AST to output Javascript to
-  // another step, if steps after this will mutate the AST again
-  const code: string = recast.prettyPrint(ast, {
-    tabWidth: 2
-  }).code;
 
   const indexTpl = "./src/templates/legacy_app.ejs";
   const destJS = `${dest}/src/javascripts/legacy_app.js`;
-  editor.copyTpl(indexTpl, destJS, { code });
+  editor.copyTpl(indexTpl, destJS, { code, imports });
 };
