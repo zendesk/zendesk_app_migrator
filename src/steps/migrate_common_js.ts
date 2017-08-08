97c7996f43da31b63b8e44e93ad2019f0ecb4406
Stefan Vizzari <svizzari@zendesk.com>
2017-08-08 15:22:28 +1000

    refactor(migrate_common_js): actually check for common js modules

--- src/steps/migrate_common_js.ts
+++ src/steps/migrate_common_js.ts
@@ -1,12 +1,13 @@
 import { Map } from "immutable";
+import * as fs from "fs";
 
 export default async (options: Map<string, any>) => {
   const src = options.get("src");
   const dest = options.get("dest");
   const editor = options.get("editor");
 
-  // Test whether the app has any CommonJS modules
-  if (editor.exists(`${src}/lib`)) {
-    editor.copy(`${src}/**`, `${dest}/src/javascripts/lib/`);
+  if (fs.existsSync(`${src}/lib/`)) {
+    editor.copy(`${src}/lib/**`, `${dest}/src/javascripts/lib/`);
+    return options.set("hasCommonJS", true);
   }
 };
