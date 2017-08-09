5a06d94a4e38fd1795ed70f6e0842c91cc3b2bb1
Stefan Vizzari <svizzari@zendesk.com>
2017-08-09 11:23:52 +1000

    refactor(yarn): set up an offline cache for migration

--- src/steps/setup_app_scaffold.ts
+++ src/steps/setup_app_scaffold.ts
@@ -1,4 +1,5 @@
 import { Map } from "immutable";
+import * as path from "path";
 
 export default async (options: Map<string, any>) => {
   const src = options.get("src");
@@ -8,6 +9,29 @@ export default async (options: Map<string, any>) => {
   editor.copy("./node_modules/app_scaffold/**", dest, {
     globOptions: { dot: true }
   });
-  const eslintConfig = editor.readJSON(`${dest}/.eslintrc`);
-  editor.writeJSON(`${dest}/.eslintrc`, Map(eslintConfig).set("root", true));
+  const eslintConfig: Map<string, any> = Map(
+    editor.readJSON(`${dest}/.eslintrc`)
+  );
+  // TODO: Move this custom config directly to the Scaffold?
+  // It _may_ be OK to leave it here, as it _may_ be OK for the
+  // Migrator to be more permissive, in the interests of getting
+  // a migrated v1 app working as much as possible.
+  const migratorConfig = eslintConfig.merge({
+    root: true,
+    globals: {
+      helpers: true,
+      services: true,
+      Base64: true
+    }
+  });
+  editor.writeJSON(`${dest}/.eslintrc`, migratorConfig);
+  // FIXME?: Would it be better to run actuall Yarn commands in the
+  // dest directory to generate the .yarnrc file? (This is _potentially_ easier)
+  const yarnrcTpl = "./src/templates/yarnrc.ejs";
+  const destFile = `${dest}/.yarnrc`;
+  const offlineCachePath = `${path.resolve(
+    __dirname,
+    "../../"
+  )}/npm-packages-offline-cache`;
+  editor.copyTpl(yarnrcTpl, destFile, { offlineCachePath });
 };
