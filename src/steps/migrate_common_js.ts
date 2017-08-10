0ef94dcc2bdcdd8e29d77f3edd01c630987a5d46
Stefan Vizzari <svizzari@zendesk.com>
2017-08-10 16:54:42 +1000

    refactor(migrate_common_js): fix paths in Common JS modules to be relative

--- src/steps/migrate_common_js.ts
+++ src/steps/migrate_common_js.ts
@@ -1,13 +1,72 @@
 import { Map } from "immutable";
-import * as fs from "fs";
+import * as recast from "recast";
+import { existsSync } from "fs";
+import { dirname, sep, join } from "path";
+import * as prettier from "prettier";
+const n = recast.types.namedTypes;
+const b = recast.types.builders;
 
 export default async (options: Map<string, any>) => {
   const src = options.get("src");
   const dest = options.get("dest");
   const editor = options.get("editor");
 
-  if (fs.existsSync(`${src}/lib/`)) {
-    editor.copy(`${src}/lib/**`, `${dest}/src/javascripts/lib/`);
+  if (existsSync(`${src}/lib/`)) {
+    const process = (code: Buffer | string, filePath: string) => {
+      const ast = recast.parse(code.toString());
+      const fileDir = dirname(filePath);
+      const dirs = fileDir.split(sep);
+      const tryResolve = (p: string): boolean => {
+        try {
+          const resolved = require.resolve(p);
+          return true;
+        } catch (error) {
+          return false;
+        }
+      };
+
+      recast.types.visit(ast, {
+        visitCallExpression(path) {
+          const node = path.node;
+          // Check whether there are any calls to `require("some_module")`
+          if (
+            n.Identifier.check(node.callee) &&
+            node.callee.name === "require"
+          ) {
+            // Keep the path to the module
+            let modulePath: string = node.arguments[0].value;
+            // Check whether the module can be resolved by node
+            // based on the existing path value
+            if (tryResolve(join(fileDir, modulePath))) {
+              node.arguments[0].value =
+                modulePath.indexOf("./") === 0 ? modulePath : `./${modulePath}`;
+            } else {
+              let found = false;
+              let newModulePath = modulePath;
+              while (dirs.pop() !== "lib") {
+                const remainingDirs = dirs.join(sep);
+                const lookInDir = join(remainingDirs, newModulePath);
+                newModulePath = `../${newModulePath}`;
+                if (tryResolve(lookInDir)) {
+                  node.arguments[0].value = newModulePath;
+                  found = true;
+                  break;
+                }
+              }
+              if (!found) {
+                throw new Error(
+                  `Unable to resolve Common JS module: ${modulePath}`
+                );
+              }
+            }
+          }
+          this.traverse(path);
+        }
+      });
+      code = recast.print(ast).code;
+      return prettier.format(code);
+    };
+    editor.copy(`${src}/lib/**`, `${dest}/src/javascripts/lib/`, { process });
     return options.set("hasCommonJS", true);
   }
 };
