428eac62bce16ef9a0b60c78dcacab7d0d94d32a
Stefan Vizzari <svizzari@zendesk.com>
2017-08-29 15:20:31 +1000

    feature(zdMenus): inject css, if needed

--- src/steps/migrate_app_css.ts
+++ src/steps/migrate_app_css.ts
@@ -4,10 +4,18 @@ export default async (options: Map<string, any>) => {
   const src = options.get("src");
   const dest = options.get("dest");
   const editor = options.get("editor");
+  const importZendeskMenus: boolean = options.get("importZendeskMenus", false);
+
+  if (importZendeskMenus) {
+    editor.copy(
+      "./src/templates/zendesk_menus.css",
+      `${dest}/src/stylesheets/zendesk_menus.css`
+    );
+  }
 
   // Copy and reformat `./app.css`
   const cssTpl = "./src/templates/css.ejs";
   const destCSS = `${dest}/src/stylesheets/app.scss`;
   const css = editor.read(`${src}/app.css`, "/* Add CSS here... */");
-  editor.copyTpl(cssTpl, destCSS, { css });
+  editor.copyTpl(cssTpl, destCSS, { css, importZendeskMenus });
 };
